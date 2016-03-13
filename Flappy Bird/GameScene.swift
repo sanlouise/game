60b8a40f7c238b8ec067f267f9154c2def092daa
Sandra Adams-Hallie <Sandra@sandras-macbook-pro.local>
2016-03-13 19:50:11 +0100

    Add SKPhysics for gravity

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -17,24 +17,29 @@ class GameScene: SKScene {
     //Equivalent viewDidLoad
     override func didMoveToView(view: SKView) {
         
-//       Make sure to first set the code for the background so that the animation will not be overrun and thus invisible.
+        //  Make sure to first set the code for the background so that the animation will not be overrun and thus invisible.
         
         let backgroundTexture = SKTexture(imageNamed: "bg.png")
-        // Add texture to background.
-        background = SKSpriteNode(texture: backgroundTexture)
-        //Position and size background
-        background.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-        background.size.height = self.frame.height
-        background.zPosition = -1
+        // Make the background move to the left to create the illusion that the bird is moving right.
+        let moveBackground = SKAction.moveByX(-backgroundTexture.size().width, y: 0, duration: 9)
+        // To make sure the background 'does not run out', it should repeat after the view is finished.
+        // Jumps the background back to its original position. Duration 0 to move immediately.
+        let replaceBackground = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0)
+        let moveBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([moveBackground, replaceBackground]))
         
-        // Make the background move to the left (by ten degrees) to create the illusion that the bird is moving right.
-        let moveBackground = SKAction.moveByX(-10, y: 0, duration: 0.2)
-        let moveBackgroundForever = SKAction.repeatActionForever(moveBackground)
-        // Apply to the background
-        background.runAction(moveBackgroundForever)
-        
-        self.addChild(background)
-        
+        // To get rid of the greyness after the background has finished.
+        for var i: CGFloat = 0; i<3; i++ {
+            // Add texture to background.
+            background = SKSpriteNode(texture: backgroundTexture)
+            //Position and size background. Because there are 3 i instances, there won't be a grey gap in between.
+            background.position = CGPoint(x: backgroundTexture.size().width/2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            background.size.height = self.frame.height
+            background.zPosition = -1
+            
+            // Apply to the background
+            background.runAction(moveBackgroundForever)
+            self.addChild(background)
+        }
         
         // When using spriteKit, define objects that will be animated as a Texture.
         let birdTexture = SKTexture(imageNamed: "flappy1.png")
@@ -50,10 +55,17 @@ class GameScene: SKScene {
         bird.zPosition = 1
         // Add animation to bird Sprite
         bird.runAction(birdFlapForever)
+        // Arrange the gravity/ collisions with other objects. Half the height of the bird image should be affected.
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height/2)
+        // Apply gravity.
+        bird.physicsBody!.dynamic = true
+        // Create a ground property so that the bird does not fall off the screen.
         // Add the Node to the screen/scene.
         self.addChild(bird)
         
         
+
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
